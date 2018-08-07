a354cf3e361797ddee1ab4f38312673e939a7b1d
theja.vempalli <theja.vempalli@workday.com>
2018-08-06 22:20:51 -0700

    update the main entry point class

--- src/test/java/SearchMainTest.java
+++ src/test/java/SearchMainTest.java
@@ -5,8 +5,4 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SearchMainTest {
-    @Test public void testSomeLibraryMethod() {
-        SearchMain classUnderTest = new SearchMain();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
 }
